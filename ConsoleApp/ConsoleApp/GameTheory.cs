c1cb911ac1ceaecb94eed6a0ec6c675032827909
alegalone <alegalone@gmail.com>
2026-02-17 11:47:25 +0800

    建立專案、一個簡單的決策

---
+++ ConsoleApp/ConsoleApp/GameTheory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    public class GameTheory
+    {
+    }
+
+    public class DecisionMaker
+    {
+        public string Name { get; set; }
+        Func<GameInfo, Dictionary<string, object>>  _evaluate;
+        public DecisionMaker(string name, Func<GameInfo, Dictionary<string, object>> evaluate)
+        {
+            this.Name = name;
+            this._evaluate = evaluate;
+        }
+
+        public Dictionary<string, object> Decision(GameInfo info) {
+            return _evaluate(info);
+        }
+    }
+
+    public class GameInfo
+    {
+        public List<object> Json { get; set; } 
+    }
+
+}
---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleApp;
+
+Console.WriteLine("Hello, World!");
+
+var a = Init_A();
+Console.WriteLine(ShowDecission(a.Decision(new GameInfo { Json = GetJ() })));
+
+DecisionMaker Init_A()
+{
+    return new DecisionMaker("A", info =>
+    {
+        return info.Json.Cast<Dictionary<string, object>>().Where(x => x["object"] == "A").OrderByDescending(x => x["value"]).FirstOrDefault();
+    });
+}
+string ShowDecission(Dictionary<string, object> result)
+{
+    return (result["value"]).ToString();
+}
+static List<object> GetJ()
+{
+    return  new List<object>
+    {
+        new Dictionary<string, object>
+        {
+            {"object", "A"},
+            {"value", 10}
+        },
+        new Dictionary<string, object>
+        {
+            {"object", "A"},
+            {"value", 5}
+        },
+    };
+}
